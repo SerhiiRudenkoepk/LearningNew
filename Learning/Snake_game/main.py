db35c4f4da05108b2f083fe7f554c4cfed535d4d
Luriando <196345190+Luriand@users.noreply.github.com>
2025-12-07 02:01:05 +0100

    Implemented food class for the Snake game, allowing food to spawn randomly on the screen. Updated main game loop to refresh food position when the snake collides with it. Refactored snake movement logic for improved readability.

--- Learning/Snake_game/main.py
+++ Learning/Snake_game/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 import time
 import snake
+import food
+
 
 screen = Screen()
 screen.setup(800, 600)
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 
 snake = snake.Snake()
+food = food.Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,9 +27,12 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    for snk in snake.segments:
-        snk.forward(20)
-        time.sleep(0.1)
+    snake.move()
+    time.sleep(0.1)
+    
+    
+    if snake.head.distance(food) < 14:
+        food.refresh()
 
 
 
